3f95e4cf3228c873711d7b7157cc747502d2e044
Nurbek <nurbeknesipbai000@gmail.com>
2021-06-09 03:12:44 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Nurbek11/dz3"
+)
+
+func main() {
+	//case-changer
+	fmt.Println(dz3.CaseChanger("OneTechnologies"))
+	//find quadratic root
+	dz3.QuadraticRoot(3, 4, 5)
+	//get uuid
+	dz3.Uuid()
+}
